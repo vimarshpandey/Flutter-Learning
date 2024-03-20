f0e192fc1dab329f5d999b1602c4b43d024f619d
Vimarsh Pandey <vimarsh240501@gmail.com>
2024-03-21 01:52:03 +0530

    First program

--- test_app/lib/main.dart
+++ test_app/lib/main.dart
@@ -1,4 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() =>
-  runApp(const MaterialApp(home: Text("Hello World")));
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 72, 172, 222),
+          title: const Center(
+            child: Text("Hello World"),
+          ),
+        ),
+        backgroundColor: const Color.fromARGB(255, 3, 31, 46),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/img1.jpg'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
