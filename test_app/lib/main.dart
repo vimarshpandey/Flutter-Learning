a79641b8a924cca4fa5ad03b529e24401fdd154c
Vimarsh Pandey <vimarsh240501@gmail.com>
2024-03-25 14:49:16 +0530

    basic row column

--- test_app/lib/main.dart
+++ test_app/lib/main.dart
@@ -1,22 +1,55 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.teal,
         appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 72, 172, 222),
-          title: const Center(
-            child: Text("Hello World"),
-          ),
+          centerTitle: true,
+          backgroundColor: Colors.indigoAccent,
+          title: const Text("Hello"),
         ),
-        backgroundColor: const Color.fromARGB(255, 3, 31, 46),
-        body: const Center(
-          child: Image(
-            image: AssetImage('images/img1.jpg'),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                height: double.infinity,
+                width: 100,
+                color: Colors.red,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              Container(
+                height: double.infinity,
+                width: 100,
+                color: Colors.blue,
+              ),
+            ],
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
